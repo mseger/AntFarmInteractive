32a2bc5ecc01549ac67fdd71eddf99b5f0a5d90e
Shane Moon <smoon@shanemoon.(none)>
2012-11-25 18:34:38 -0500

    migrated the html files to html.erb to rails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 AntFarmInteractive::Application.routes.draw do
-	resources :projects do
-		resources :comments
-	end
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
+  resources :projects do
+    resources :comments
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +58,7 @@ AntFarmInteractive::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
