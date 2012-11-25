cb0a1fbe04c7d14f75190f74469284ba24e6c65d
Shane Moon <smoon@shanemoon.(none)>
2012-11-25 12:19:21 -0500

    adding rails. will migrate MA's html files later today

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AntFarmInteractive::Application.config.secret_token = '83d788c7e4c14b88e200d9e632ab5b00c2c40c62daeae53b2154210068fc1cf9bc7fc0d722bbf46690a903819ce26ec6b0b08dfffe49addf62e67f6d05508f82'
