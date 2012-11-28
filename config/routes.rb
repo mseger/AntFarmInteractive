5cf0990e6849acb8e6f61d4757405205c1f03f3b
Shane Moon <smoon@shanemoon.(none)>
2012-11-28 16:01:12 -0500

    added preview

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ AntFarmInteractive::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
+  root :to => 'pages#preview'
 
   # See how all your routes lay out with "rake routes"
 
